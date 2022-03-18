1e18d0f09f7ad839d2eeb322e9dcdf445715f6bd
Doic <michalakradek2@gmail.com>
2022-03-18 15:53:31 +0100

    Add file with enums

---
+++ Cyberpunk Text Game/enums.h
@@ -0,0 +1,24 @@
+#pragma once
+
+enum consoleColors
+{
+	defaultColor = 15,
+	item = 3,
+	dialogue = 7,
+	narration = 8,
+	dia_robot = 9,
+	green = 10,
+	lightblue = 11,
+	red = 12,
+	yellow = 14
+};
+
+enum language { en = 1, pl = 2 };
+
+enum attitude { neutral, angry, hostile, friendly };
+
+enum status { normal, unconscious, dead };
+
+enum itemType { NONE, WEAPON, ARMOR, FOOD, OTHER, QUEST };
+
+enum armorType { HEAD, CHEST, ARMS, LEGS };
