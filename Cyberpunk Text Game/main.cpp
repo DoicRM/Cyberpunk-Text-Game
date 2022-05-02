f6f41d6705ac39efb236cb200385c9567d8d4dd9
Doic <michalakradek2@gmail.com>
2022-05-02 12:51:31 +0200

    used new functions to console management

--- Cyberpunk Text Game/main.cpp
+++ Cyberpunk Text Game/main.cpp
@@ -2,14 +2,15 @@
 
 int main()
 {   
-    int defaultEncoding = GetConsoleOutputCP();
-    SetConsoleOutputCP(65001); // set console encoding to UTF-8
     srand((unsigned)time(NULL));
+    UINT defaultInputEncoding = GetConsoleCP();
+    UINT defaultOutputEncoding = GetConsoleOutputCP();
+    Console::initConsole("Nocturnal - Demo Build", 65001 /* UTF-8 */);
     Logger::setup(false);
     Game::init();
     Game::game[0].run();
     Logger::cleanup();
     Game::game.clear();
-    SetConsoleOutputCP(defaultEncoding); // set default console encoding
+    Console::resetConsoleEncoding(defaultInputEncoding, defaultOutputEncoding);
     exit(0);
 }
