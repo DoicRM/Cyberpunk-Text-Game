501bb455e7465ab7ded9d26691fd0935e2ec4f91
Doic <michalakradek2@gmail.com>
2022-05-02 10:19:39 +0200

    fixed console encoding

--- Cyberpunk Text Game/main.cpp
+++ Cyberpunk Text Game/main.cpp
@@ -3,13 +3,13 @@
 int main()
 {   
     int defaultEncoding = GetConsoleCP();
-    SetConsoleCP(65001); // set console encoding to UTF-8
+    SetConsoleOutputCP(65001); // set console encoding to UTF-8
     srand((unsigned)time(NULL));
     Logger::setup(false);
     Game::init();
     Game::game[0].run();
     Logger::cleanup();
     Game::game.clear();
-    SetConsoleCP(defaultEncoding); // set default console encoding
+    SetConsoleOutputCP(defaultEncoding); // set default console encoding
     exit(0);
 }
