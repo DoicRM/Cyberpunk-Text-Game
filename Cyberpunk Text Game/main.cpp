082c008380dfaeb0a058ebbdd0a479be44206c1a
Doic <michalakradek2@gmail.com>
2022-05-03 12:00:19 +0200

    changed console name

--- Cyberpunk Text Game/main.cpp
+++ Cyberpunk Text Game/main.cpp
@@ -5,7 +5,7 @@ int main()
     srand((unsigned)time(NULL));
     UINT defaultInputEncoding = GetConsoleCP();
     UINT defaultOutputEncoding = GetConsoleOutputCP();
-    Console::initConsole("Nocturnal - Demo Build", 65001 /* UTF-8 */);
+    Console::initConsole("Nocturnal [Demo Build]", 65001 /* UTF-8 */);
     Logger::setup(false);
     Game::init();
     Game::game[0].run();
