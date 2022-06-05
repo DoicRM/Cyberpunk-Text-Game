55d93431441cefc7f8aa169b8bd32eb6b2c86209
Doic <michalakradek2@gmail.com>
2022-06-05 19:28:42 +0200

    separate console utilities to another file

--- Cyberpunk Text Game/src/utilities/utilities.hpp
+++ Cyberpunk Text Game/src/utilities/utilities.hpp
@@ -1,29 +1,15 @@
 #pragma once
 #include "../utilities/logger/logger.hpp"
+#include "../utilities/console/console.hpp"
 #include <iostream>
 #include <fstream>
 #include <thread>
 #include <cmath>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h>
-#include <windows.h>
 #include <string>
 #include <map>
 #include <limits>
-
-enum ConsoleColor
-{
-	CC_Default = 15,
-	CC_Item = 3,
-	CC_Dialogue = 7,
-	CC_Narration = 8,
-	CC_Robot = 9,
-	CC_Green = 10,
-	CC_Lightblue = 11,
-	CC_Red = 12,
-	CC_Yellow = 14
-};
 
 enum GameLanguage
 {
@@ -58,37 +44,6 @@ enum ItemType
 enum ArmorType
 {
 	HEAD, CHEST, ARMS, LEGS
-};
-
-class Console
-{
-public:
-	static void initConsole(LPCSTR lpConsoleTitle, UINT wCodePageID);
-	static void preventResizeConsole();
-	static void setConsoleTitle(LPCSTR lpConsoleTitle);
-	static void clearScreen(); // czyszczenie ekranu
-	static void setConsoleColor(int color = CC_Default); // zmiana koloru tekstu i tła konsoli
-	static void resetConsoleColor();
-	static void resetConsoleEncoding(UINT input, UINT output);
-
-	//	1 czarny tekst na czarnym tle
-	//	1 ciemno niebieski tekst na czarnym tle
-	//	2 ciemno zielony tekst na czarnym tle
-	//	3 zielono-niebieski tekst na czarnym tle
-	//	4 ciemno czerwony tekst na czarnym tle
-	//	5 magenta ciemna tekst na czarnym tle
-	//	6 oliwkowy tekst na czarnym tle
-	//	7 jasny szary tekst na czarnym tle
-	//	8 szary tekst na czarnym tle
-	//	9 niebieski tekst na czarnym tle
-	//	10 zielony tekst na czarnym tle
-	//	11 jasno niebieski tekst na czarnym tle
-	//	12 czerwony tekst na czarnym tle
-	//	13 magenta tekst na czarnym tle
-	//	14 żółty tekst na czarnym tle
-	//	15 biały tekst na czarnym tle
-
-	static void waitForUserInput();
 };
 
 class Display
